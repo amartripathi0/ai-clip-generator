e34322cba6d050644f086e322ef6d121bd4938db
amartripathi0 <theamartripathi@gmail.com>
2024-12-30 10:12:47 +0530

    add config in upload route

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -5,6 +5,12 @@ import { NextRequest } from "next/server";
 import {  videoUpload } from "@/utils/uploadVideo";
 import { getTranscript } from "@/utils/getTranscript";
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 const uploadDir = path.join(process.cwd(), "public", "uploads");
 
 if (!fs.existsSync(uploadDir)) {
